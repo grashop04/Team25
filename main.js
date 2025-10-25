2477c89425bb387297b334a31b13aad80918ef2e
callum neilson <c411um.17@gmail.com>
2025-10-25 15:17:44 +0100

    Test

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let character = document.getElementById('character');
 let spike = document.getElementById('spike');
+let spikeUP = document.getElementById('spikeUP');
 let seagull = document.getElementById('seagull');
 let gravflip = false;
 
+//gravity toggle
 function toggleGrav() {
   gravflip = !gravflip;
   character.classList.toggle('gravflip', gravflip);
@@ -19,10 +21,14 @@ let checkDead = setInterval(function(){
   let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
   let spikeLeft = parseInt(window.getComputedStyle(spike).getPropertyValue('left'));
   let seagullLeft = parseInt(window.getComputedStyle(seagull).getPropertyValue('left'));
+  let spikeUPLeft = parseInt(window.getComputedStyle(spikeUP).getPropertyValue('left'));
   if (
   (spikeLeft < 20 && spikeLeft > 0 && characterTop >= 130) ||
-  (seagullLeft < 20 && seagullLeft > 0 && characterTop >= 130)
+  (seagullLeft < 20 && seagullLeft > 0 && characterTop >= 130) ||
+  (spikeUPLeft < 20 && spikeUPLeft > 0 && characterTop <= 130)
 ) {
+  spikeUP.style.animation = 'none';
+  spikeUP.style.display = 'none';
   spike.style.animation = 'none';
   spike.style.display = 'none';
   seagull.style.animation = 'none';
